13c51170a794a5b521282d8c24a55f5774664360
Maurice Johnson <maurice.johnson9911@gmail.com>
2023-03-16 14:16:58 -0400

    removing test index and adding videos endpoint

--- src/main/java/com/software/alpha/controller/MainController.java
+++ src/main/java/com/software/alpha/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.software.alpha.controller;
 
+import com.software.alpha.model.YoutubeResults;
 import com.software.alpha.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +14,8 @@ public class MainController {
     @Autowired
     private MainService mainService;
 
-    @GetMapping("/")
-    public String getIndex(){
-        return mainService.getIndex();
+    @GetMapping("/videos")
+    public @ResponseBody ResponseEntity<YoutubeResults> getRecentVideos(){
+        return mainService.getVideos();
     }
 }
